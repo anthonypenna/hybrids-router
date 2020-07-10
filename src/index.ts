5a404165c82541b1ae735e3bae500b0d6e70182b
Anthony Penna <anthony.penna@musement.com>
2020-07-10 16:18:55 +0200

    chore: refactor, update docs

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,39 @@
-import { Hybrids, html } from 'hybrids';
-import { getCurrentPath, hash } from './util';
+import { Hybrids, html, render } from 'hybrids';
+import { getCurrentPath, hash, unhash } from './util';
 import { RouterOptions, HybridRouter } from './interfaces';
+
+const template = (host: HybridRouter) => {
+  const historyMode = host.mode === 'history';
+  const matchingRoute = historyMode
+    ? host.routes.find((route) => unhash(route.path) === host.currentPath)
+    : host.routes.find((route) => hash(route.path) === host.currentPath);
+
+  return matchingRoute.component || html``;
+};
 
 function Router(options: RouterOptions): Hybrids<HybridRouter> {
   return {
     mode: options.mode || 'history',
     routes: options.routes || [],
     currentPath: getCurrentPath(window),
-    render: (host: HybridRouter) => {
-      const historyMode = host.mode === 'history';
-      const matchingRoute = historyMode
-        ? host.routes.find((route) => route.path === host.currentPath)
-        : host.routes.find((route) => hash(route.path) === host.currentPath);
-
-      return matchingRoute.component || html``;
-    },
+    render: render(template, { shadowRoot: options.shadowRoot }),
   };
 }
 
 export function push(host: HybridRouter, path: string): void {
-  if (getCurrentPath(window) === path) return;
+  const hashed = hash(path);
+  const unhashed = unhash(path);
+  const currentPath = getCurrentPath(window);
+
+  if (currentPath === hashed || currentPath === unhashed) return;
 
   const { state } = window.history;
   const { pathname } = window.location;
   const historyMode = host.mode === 'history';
 
-  host.currentPath = historyMode ? path : hash(path);
+  host.currentPath = historyMode ? unhashed : hashed;
   window.history.pushState(state, pathname, host.currentPath);
-
-  window.addEventListener('popstate', () => {
-    const currentPath = getCurrentPath(window);
-    host.currentPath = currentPath;
-  });
+  window.addEventListener('popstate', () => (host.currentPath = getCurrentPath(window)));
 }
 
 export default Router;
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,18 +1,19 @@
-import { UpdateFunctionWithMethods } from 'hybrids'
-import { RouterMode } from '../types'
+import { UpdateFunctionWithMethods } from 'hybrids';
+import { RouterMode } from '../types';
 
 export interface Route {
-  path: string
-  component: UpdateFunctionWithMethods<HTMLElement>
+  path: string;
+  component: UpdateFunctionWithMethods<HTMLElement>;
 }
 
 export interface RouterOptions {
-  mode: RouterMode
-  routes: Route[]
+  mode: RouterMode;
+  routes: Route[];
+  shadowRoot: boolean;
 }
 
 export interface HybridRouter extends HTMLElement {
-  mode: RouterMode
-  routes: Route[]
-  currentPath: string
+  mode: RouterMode;
+  routes: Route[];
+  currentPath: string;
 }
